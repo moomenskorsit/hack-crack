90a36e5a1524844ad439d1e2001d3c8970609d48
Marijn <m.oomens@korsit.com>
2023-06-08 13:31:41 +0200

    grafiek

--- components/DisplayCard.tsx
+++ components/DisplayCard.tsx
@@ -1,10 +1,67 @@
 import { FC } from "react"
+import { faker } from '@faker-js/faker';
+
+
+import {Chart, Line} from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+export const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart',
+      },
+    },
+  };
+  
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  
+  export const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: labels.map(() => faker.number.int({ min: -1000, max: 1000 })),
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: 'Dataset 2',
+        data: labels.map(() => faker.number.int({ min: -1000, max: 1000 })),
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
 
 const DisplayCard: FC = () => {
     return (
         <div className="w-4/5 h-4/5 bg-cyan-900 rounded-2xl flex flex-col">
-            <div className="p-8 flex justify-center">
+            <div className="p-8 flex flex-col justify-center items-center">
                 <h1 className="font-bold text-2xl">Order Verification Dashboard</h1>
+                <Line options={options} data={data} />
             </div>
         </div>
     )
