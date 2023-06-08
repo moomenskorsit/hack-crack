b07780e0deb7d6574b8c3abd5ca9ed362e6253af
Marijn <m.oomens@korsit.com>
2023-06-08 11:09:03 +0200

    meuk

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="OLRT order verification data" />
+      </Head>
       <body>
         <Main />
         <NextScript />
